302fa245095984dae75bc7866be4c28f451db4ee
lilblupig <lilblue2005@hotmail.co.uk>
2021-04-17 10:38:54 +0000

    Print parsed data to console.

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('data').innerHTML = this.responseText;
-    }
-};
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-xhr.open('GET','https://ci-swapi.herokuapp.com/api/');
+    xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
 
-xhr.send();
+    xhr.send();
+}
+
+getData(function(data) {
+    console.log(data);
+});
